ab53d4628d67a06f5a0bd01886ac0ed753aa0f6d
SuthidaDamchum <suthida.damchum@hotmail.com>
2025-09-19 08:58:24 +0200

    Update Program.cs

--- Labb1 - Algoritmer (G-uppgift)/Program.cs
+++ Labb1 - Algoritmer (G-uppgift)/Program.cs
@@ -1,40 +1,47 @@
 ﻿
 
 Console.Write("Mata in en text: ");
-string long1 = Console.ReadLine();
+string inmatning = Console.ReadLine();
+
 long summa = 0;
-for (int i = 0; i < long1.Length; i++)
+for (int i = 0; i < inmatning.Length; i++)
 {
-    if (!int.TryParse(long1[i].ToString(), out int number))
+    if (!int.TryParse(inmatning[i].ToString(), out int number))
     {
         continue;
     }
-    for (int j = i + 1; j < long1.Length; j++)
+    for (int j = i + 1; j < inmatning.Length; j++)
     {
-        if (!int.TryParse(long1[j].ToString(), out int number2))
+        if (!int.TryParse(inmatning[j].ToString(), out int number2))
         {
             break;
         }
-        if (long1[i] == long1[j])
+        if (inmatning[i] == inmatning[j])
         {
-            var start = "";
+            string start = "";
+
             if (i != 0)
-                start = long1.Substring(0, i);
-            var found = long1.Substring(i, j - i + 1);
-            summa += Convert.ToInt64(found);
+                start = inmatning.Substring(0, i);
+
+            string found = inmatning.Substring(i, j - i + 1);
+
+            summa = summa + Convert.ToInt64(found);
             string end = "";
-            if (j + 1 < long1.Length)
-                end = long1.Substring(j + 1);
+
+            if (j + 1 < inmatning.Length)
+                end = inmatning.Substring(j + 1);
+
             Console.Write(start);
             Console.ForegroundColor = ConsoleColor.Red;
             Console.Write(found);
             Console.ResetColor();
             Console.Write(end);
             Console.WriteLine();
+
             break;
         }
     }
-
 }
-Console.Write(summa);
+Console.Write("Total" + " = " + summa);
 Console.ResetColor();
+
